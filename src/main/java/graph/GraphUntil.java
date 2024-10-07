9d423533ac30ce9a525713c3dfa94613818bc73a
Yichun Zhao <zhaoyichun@hotmail.com>
2024-10-07 22:00:18 +0200

    bfs traverse graph nodes

--- src/main/java/graph/GraphUntil.java
+++ src/main/java/graph/GraphUntil.java
@@ -57,4 +57,54 @@ public class GraphUntil {
             }
         }
     }
+
+    /**
+     * Performs a Breadth First Search (BFS) on the given graph starting from the specified vertex.
+     *
+     * @param graph       the graph to perform BFS on
+     * @param startVertex the starting vertex for the BFS
+     * @throws IllegalArgumentException if the startVertex is not found in the graph
+     *                                  This method logs the process of visiting each vertex. It marks the start vertex as visited,
+     *                                  then uses a queue to explore each adjacent vertex. If an adjacent vertex has not been visited,
+     *                                  it is marked as visited and its adjacent vertices are added to the queue for further exploration.
+     */
+    public void bfsIterative(Graph graph, Vertex startVertex) {
+        if (graph == null) {
+            throw new IllegalArgumentException("graph cannot be null");
+        }
+
+        if (startVertex == null || graph.getVertex(startVertex.getLabel()) == null) {
+            throw new IllegalArgumentException("startVertex not found in the graph");
+        }
+
+        log.info("Breadth First Search (BFS) starting from: " + startVertex.getLabel());
+        startVertex.setVisited(true);
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        startVertex.getAdjacentVertexes().forEach(queue::offer);
+
+        // while the queue is not empty
+        while (!queue.isEmpty()) {
+            System.out.println("queue: " + queue);
+            // get the next vertex
+            String vertexLabel = queue.poll();
+            Vertex vertex = graph.getVertex(vertexLabel);
+
+            // if the vertex has not been visited
+            if (!vertex.isVisited()) {
+                // visit the vertex
+                System.out.println("Visiting vertex: " + vertex.getLabel());
+                vertex.setVisited(true);
+
+                // get the adjacent vertexes of the vertex, add them to the queue one by one
+                vertex.getAdjacentVertexes().forEach(av -> {
+                    Vertex adjacentVertex = graph.getVertex(av);
+                    if (!adjacentVertex.isVisited()) {
+                        queue.offer(adjacentVertex.getLabel());
+                    }
+                });
+            }
+        }
+    }
 }
